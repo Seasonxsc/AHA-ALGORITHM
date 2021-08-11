f01c3861f7b719b0d7cb404306de570a63d867ee
Seasonxsc <201821541853@smail.xtu.edu.cn>
2021-08-11 21:40:12 +0800

    完成隊列和棧結構

---
+++ ch02/stack/stack.go
@@ -0,0 +1,32 @@
+package main
+
+type Stack struct {
+	stack []int
+}
+
+func (s *Stack) size() int {
+	return len(s.stack)
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.stack) == 0
+}
+
+func (s *Stack) push(element int) {
+	s.stack = append(s.stack, element)
+}
+
+func (s *Stack) pop() int {
+	tail := s.stack[len(s.stack)-1]
+	s.stack = s.stack[:len(s.stack)-1]
+	return tail
+}
+
+func (s *Stack) top() int {
+	return s.stack[len(s.stack)-1]
+}
+
+func main() {
+	s := Stack{stack: []int{}}
+
+}
